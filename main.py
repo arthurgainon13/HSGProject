d1b38c46d489690ffe6b507f08aff0fb7f6eef5d
arthurgainon13 <arthur.gainon@student.unisg.ch>
2024-11-30 15:15:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# ---------------------------------------------------------
+# RSI Backtesting Tool - Main Application
+#
+# Language: Python
+# Author: Samuel
+# Date Created: November 28, 2024
+# Description: This is the entry point of the RSI Backtesting Tool. It
+#              initializes and launches the application by creating
+#              an instance of the `BacktestApp` class from `app_ui.py`.
+# Assistance: This structure and modularization were inspired by
+#             ChatGPT, an AI language model by OpenAI.
+
+from app_ui import BacktestApp
+
+if __name__ == '__main__':
+    app = BacktestApp()
+    app.mainloop()
+
 
