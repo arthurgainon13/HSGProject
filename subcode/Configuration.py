fabdc940dfaace4b170bbe95a4f9bb2ff73b2430
arthurgainon13 <arthur.gainon@student.unisg.ch>
2024-12-05 20:53:25 +0100

    Update Configuration.py

--- subcode/Configuration.py
+++ subcode/Configuration.py
@@ -7,6 +7,7 @@
 #              an instance of the `BacktestApp` class from `app_ui.py`.
 # Assistance: This structure and modularization were inspired by
 #             ChatGPT, an AI language model by OpenAI.
+# ---------------------------------------------------------
 
 # In this file, you can choose and modify the configuration of the code.
 # You can adapat your analysed companies as well as Dates of analysis
